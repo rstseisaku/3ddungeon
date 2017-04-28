7e702f782c6b7073a223e4e63539eda2f8503dce
eiichi-pc <eiichi-pc@EIICHI>
2017-04-28 23:15:35 +0900

    とりあえずコミット

--- unityfiles/Assets/Resources/Materials/TransitionShader.shader
+++ unityfiles/Assets/Resources/Materials/TransitionShader.shader
@@ -13,16 +13,18 @@
 		}
 		LOD 200
 
+
 		Cull Off
 		
 		CGPROGRAM
 		// Physically based Standard lighting model, and enable shadows on all light types
-		#pragma surface surf Standard fullforwardshadows alphatest:_Cutoff
+		#pragma surface surf Standard fullforwardshadows
 
 		// Use shader model 3.0 target, to get nicer looking lighting
 		#pragma target 3.0
 
 		sampler2D _MainTex;
+		float _Cutoff = 0.5f;
 
 		struct Input {
 			float2 uv_MainTex;
@@ -42,12 +44,16 @@
 			void surf(Input IN, inout SurfaceOutputStandard o) {
 			// Albedo comes from a texture tinted by color
 			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+			float ca = tex2D(_MainTex, IN.uv_MainTex).a;
 			o.Albedo = c.rgb;
 			// Metallic and smoothness come from slider variables
 			o.Metallic = _Metallic;
 			o.Smoothness = _Glossiness;
-			o.Alpha = c.a;
-			
+			if (ca > _Cutoff)
+				o.Alpha = c.a;
+			else
+				o.Alpha = 0.0f;
+
 		}
 		ENDCG
 	}
--- unityfiles/Assets/Script/TEST.cs
+++ unityfiles/Assets/Script/TEST.cs
@@ -5,30 +5,33 @@ using UnityEngine.UI;
 
 public class TEST : MonoBehaviour {
 
-    public float fill = 0;
+    public float time = 0.1f;
 
-    Image fuck;
+    Image transition;
+    float rate;
+    float temp = 0;
 
     // Use this for initialization
     void Start () {
-        fuck = GetComponent<Image>();
+        transition = GetComponent<Image>();
+        rate = 1.0f / (time * 60);
         //fuck.material.shader = Shader.Find("Transition");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fill < 1)
+        if (temp < 1)
         {
-            fill += 0.015f;
-            if (fuck.name == "Image")
+            temp += rate;
+            if (transition.name == "Image")
             {
-                fuck.material.EnableKeyword("_Cutoff");
-                fuck.material.SetFloat("_Cutoff", fill);
+                transition.material.EnableKeyword("_Cutoff");
+                transition.material.SetFloat("_Cutoff", temp);
             }
-            if (fuck.name == "Image2")
+            if (transition.name == "Image2")
             {
-                fuck.fillAmount = fill;
+                transition.fillAmount = temp;
             }
         }
     }
