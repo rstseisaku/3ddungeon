0d09307162457ec6f1d7a29fd39df06e6ce550ec
eiichi-pc <eiichi-pc@EIICHI>
2017-05-07 23:49:19 +0900

    一度全部削除

---
+++ unityfiles/Assets/Script/Sound/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SoundManager : MonoBehaviour
+{
+    static GameObject soundObj;
+    static Sound sound;
+
+    public static void SceneChangePlaySound(Variables.BGM.BgmName id)
+    {
+        soundObj = GameObject.Find(Variables.SE.soundObjName);
+        if (soundObj == null)
+        {
+            soundObj = new GameObject(Variables.SE.soundObjName);
+            sound = soundObj.AddComponent<Sound>();
+            sound.LoadSound();
+            DontDestroyOnLoad(soundObj);
+        }
+        else
+        {
+            sound = soundObj.GetComponent<Sound>();
+        }
+
+        if (Variables.__Debug.isNotBGMPlay) return;
+
+        // このシーンで流すBGMを設定
+        sound.PlayBgm(id);
+    }
+
+    public static void PlaySe(Variables.SE.SeName seId)
+    {
+        sound.PlaySe(seId);
+    }
+}
